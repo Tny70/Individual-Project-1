24af10579b0f93422a0960f8c5da5a6cca91b5da
Terence Yebuah <88135734+Tny70@users.noreply.github.com>
2022-10-02 10:36:10 +0000

    Update Airlines.java

--- Airlines.java
+++ Airlines.java
@@ -1,119 +1,161 @@
-import java.io.*;
-import java.util.*;
+import java.io.* ;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
-public class Main {
-    String startingCity;
-    String startingCountry;
-    String endCity;
-    String endCountry;
+public class Airlines {
 
-    public void reading() {
+    public static HashMap<String, ArrayList<Airlines>> airlinesMap = new HashMap<>();
+    private String airlineID;
+    private String airlineName;
+    private String airlineAlias;
+    private String airlineIataCode;
+    private String airlineIcoaCode;
+    private String airlineCallsign;
+    private String airlineCountry;
+    private String airlineActive;
+
+    public static HashMap<String, ArrayList<Airlines>> getAirlinesMap() {
+        return airlinesMap;
+    }
+
+    public String getAirlineID() {
+        return airlineID;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public String getAirlineAlias() {
+        return airlineAlias;
+    }
+
+    public String getAirlineIataCode() {
+        return airlineIataCode;
+    }
+
+    public String getAirlineIcoaCode() {
+        return airlineIcoaCode;
+    }
+
+    public String getAirlineCallsign() {
+        return airlineCallsign;
+    }
+
+    public String getAirlineCountry() {
+        return airlineCountry;
+    }
+
+    public String getAirlineActive() {
+        return airlineActive;
+    }
+
+
+    public static void setAirlinesMap(HashMap<String, ArrayList<Airlines>> airlinesMap) {
+        Airlines.airlinesMap = airlinesMap;
+    }
+
+    public void setAirlineID(String airlineID) {
+        this.airlineID = airlineID;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public void setAirlineAlias(String airlineAlias) {
+        this.airlineAlias = airlineAlias;
+    }
+
+    public void setAirlineIataCode(String airlineIataCode) {
+        this.airlineIataCode = airlineIataCode;
+    }
+
+    public void setAirlineIcoaCode(String airlineIcoaCode) {
+        this.airlineIcoaCode = airlineIcoaCode;
+    }
+
+    public void setAirlineCallsign(String airlineCallsign) {
+        this.airlineCallsign = airlineCallsign;
+    }
+
+    public void setAirlineCountry(String airlineCountry) {
+        this.airlineCountry = airlineCountry;
+    }
+
+    public void setAirlineActive(String airlineActive) {
+        this.airlineActive = airlineActive;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Airlines airlines)) return false;
+        return Objects.equals(airlineID, airlines.airlineID) && Objects.equals(airlineName, airlines.airlineName) && Objects.equals(airlineAlias, airlines.airlineAlias) && Objects.equals(airlineIataCode, airlines.airlineIataCode) && Objects.equals(airlineIcoaCode, airlines.airlineIcoaCode) && Objects.equals(airlineCallsign, airlines.airlineCallsign) && Objects.equals(airlineCountry, airlines.airlineCountry) && Objects.equals(airlineActive, airlines.airlineActive);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(airlineID, airlineName, airlineAlias, airlineIataCode, airlineIcoaCode, airlineCallsign, airlineCountry, airlineActive);
+    }
+
+    public Airlines(String airlineID, String airlineName, String airlineAlias, String airlineIataCode, String airlineIcoaCode, String airlineCallsign, String airlineCountry, String airlineActive) {
+        this.airlineID = airlineID;
+        this.airlineName = airlineName;
+        this.airlineAlias = airlineAlias;
+        this.airlineIataCode = airlineIataCode;
+        this.airlineIcoaCode = airlineIcoaCode;
+        this.airlineCallsign = airlineCallsign;
+        this.airlineCountry = airlineCountry;
+        this.airlineActive = airlineActive;
+    }
+
+    public static void reader(){
         FileReader fileViewer = null;
         BufferedReader buffReader = null;
-        try {
-            fileViewer = new FileReader("input.txt");
+        try{
+            fileViewer = new FileReader("airlines.csv");
             buffReader = new BufferedReader(fileViewer);
             String newLine;
-            String start = buffReader.readLine();
-            String[] lines = start.split(",");
-            this.startingCity = lines[0];
-            this.startingCountry = lines[1];
+            while ((newLine = buffReader.readLine()) != null) {
+                String[] airline = newLine.split(",");
+                String airlineID = airline[0];
+                String airlineName = airline[1];
+                String airlineAlias = airline[2];
+                String airlineIATA = airline[3];
+                String airlineICOA = airline[4];
+                String airlineCallsign = airline[5];
+                String airlineCountry = airline[6];
+                String airlineActive = airline[7];
 
-            String end = buffReader.readLine();
-            String[] lines2 = end.split(",");
-            this.endCity = lines2[0];
-            this.endCountry = lines2[1];
-        } catch (FileNotFoundException ex) {
-            throw new RuntimeException(ex);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+                Airlines newAirlines = new Airlines(airlineID,airlineName,airlineAlias,airlineIATA,airlineICOA,airlineCallsign,airlineCountry,airlineActive);
 
-    }
-
-        public ArrayList<Node> nodeCreator(){
-            ArrayList<Airports> startAirport = Airports.getAirportsMap().get(startingCity+startingCountry);
-            ArrayList<Node> airportNodes = new ArrayList<>();
-            for (Airports airport:startAirport){
-                Node node = new Node(null, airport, null, 0);
-                airportNodes.add(node);
-            }
-            System.out.println(airportNodes);
-            return airportNodes;
-    }
-
-    public boolean goalTest(Node child){
-        if (child.getState() ==null){
-            return false;
-        }
-        if (child.getState().getAirportCity().equals(endCity)  && child.getState().getAirportCountry().equals(endCountry)){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
-    public Node bfs() {
-        Queue<Node> frontier = new LinkedList<Node>();
-        ArrayList<Node> airportNode = nodeCreator();
-
-        frontier.addAll(airportNode);
-
-        HashSet<Node> explored = new HashSet<>();
-
-        while (frontier.size() > 0) {
-            Node initialNode = frontier.poll();
-            explored.add(initialNode);
-            if (initialNode.getState() != null) {
-                ArrayList<Routes> flights = Routes.routesMap.get(initialNode.getState().getAirportIata());
-                for (Routes action : flights) {
-                    Node child = new Node(initialNode, Airports.iataAirport.get(action.getDestinationAirportCode()), action, initialNode.getPathCost() + 1);
-                    if (!explored.contains(child) && !frontier.contains(child)) {
-                        if (goalTest(child)) {
-                            System.out.println(child);
-                            //Node.solutionPath(child);
-
-
-                            return child;
-                        }
-                        frontier.add(child);
-                    }
-
+                if (!airlinesMap.containsKey(airlineIATA)){
+                    airlinesMap.put(airlineIATA, new ArrayList<Airlines>());
+                    airlinesMap.get(airlineIATA).add(newAirlines);
+                }
+                else {
+                    airlinesMap.get(airlineIATA).add(newAirlines);
                 }
 
-            }else {;}
 
 
-        }
-        return null;
-    }
+            }
+            buffReader.close();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
 
-    //public void debug(){
-        //System.out.println(child);
-    //}
-
-    public void writer(){
-        PrintWriter pw=null;
-        try{
-            pw = new PrintWriter("output.txt");
-            pw.println("");
-            pw.println("done");
-            pw.close();
-        }catch(Exception e){
-            System.out.println(e.toString());
         }
     }
 
-    public static void main(String[] args) {
-        Main execute = new Main();
-
-        Airports.reader();
-        Airlines.reader();
-        Routes.reader();
-
-        execute.reading();
-        execute.nodeCreator();
-        execute.bfs();
+    @Override
+    public String toString() {
+        return "Airlines{" +
+                "airlineName='" + airlineName + '\'' +
+                ", airlineIataCode='" + airlineIataCode + '\'' +
+                '}';
     }
 }
